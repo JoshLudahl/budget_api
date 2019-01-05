b2553f478306b955b88a95cebccca69023247d75
Josh Ludahl <josh.ludahl@gmail.com>
2019-01-05 14:04:09 -0800

    Comments

--- api/models/budget.js
+++ api/models/budget.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 
+
+//  Will add owner when user management is added
 const budgetSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     name: String,
