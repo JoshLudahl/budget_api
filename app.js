4761943ecee0359a210e0984c6b0bfeb2c116f5a
Josh Ludahl <josh.ludahl@gmail.com>
2019-01-05 14:25:28 -0800

    Added http request logging (morgan)

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const app = express();
 const path = require('path');
+const morgan = require('morgan');
+
+app.use(morgan('dev'));
 
 mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useCreateIndex: true});
 mongoose.Promise = global.Promise;
